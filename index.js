18fca76508d34cb466e5a6e14e3e7133d61885cf
Vikram Belde <symmetriccurve@gmail.com>
2019-01-18 19:37:21 -0600

    clean up and better variable names

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-var inputRansomNote = 'Hand over the chocolate and no one gets hurt'
-var inputBook = 'NYTimes online: A toddler is hurt while sliding down the chocolate slide at new year eve fair'
+var inputRansomNote = "Hand over the chocolate and no one gets hurt"// {g: 1, m: 3 }
+var inputStr = "NYTimes online: A toddler is hurt while sliding down the chocolate slide at new year eve fair"
 
 function isRansomeNotePossible(ransomNote, book) {
-    let lookUp = {}
+    let charCount = {}
     ransomNote = ransomNote.split('').filter(nonSpace => nonSpace != ' ').map(char => char.toLowerCase())
     for (var char in ransomNote) {
-        if (lookUp[ransomNote[char]]) {
-            lookUp[ransomNote[char]] += 1
+        if (charCount[ransomNote[char]]) {
+            charCount[ransomNote[char]] += 1
         } else {
-            lookUp[ransomNote[char]] = 1
+            charCount[ransomNote[char]] = 1
         }
     }
 
-    const parsedBook = book.split('').filter(nonSpace => nonSpace != ' ').map(char => char.toLowerCase())
-    for (var char = 0; char <= parsedBook.length; char++) {
-        //for (var char in parsedBook) {
-        if (!Object.keys(lookUp).length) {
+    const parsedInputStr = book.split('').filter(nonSpace => nonSpace != ' ').map(char => char.toLowerCase())
+    for (var char = 0; char <= parsedInputStr.length; char++) {
+        //for (var char in parsedInputStr) { faster enumeration does not work as deletion the last property needs an extra iteration
+        // to delete the last property of the charCount
+        if (!Object.keys(charCount).length) {
             return 'Possible'
         } else {
-            if (lookUp.hasOwnProperty(parsedBook[char])) {
-                if (lookUp[parsedBook[char]]) {
-                    lookUp[parsedBook[char]] -= 1
-                    if (!lookUp[parsedBook[char]]) {
-                        delete lookUp[parsedBook[char]]
+            if (charCount.hasOwnProperty(parsedInputStr[char])) {
+                if (charCount[parsedInputStr[char]]) {
+                    charCount[parsedInputStr[char]] -= 1
+                    if (!charCount[parsedInputStr[char]]) {
+                        delete charCount[parsedInputStr[char]]
                     }
                 }
             }
@@ -31,4 +32,4 @@ function isRansomeNotePossible(ransomNote, book) {
     return 'Not Possible'
 }
 
-console.log(isRansomeNotePossible(inputRansomNote, inputBook))
+console.log(isRansomeNotePossible(inputRansomNote, inputStr))
