53a5d80026db03899c0b452bb7b61d7fe7f45b8e
Vikram Belde <symmetriccurve@gmail.com>
2019-01-18 19:26:45 -0600

    A better ransom message from much better news headline

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var inputRansomNote = "I need Money if you want your child back"// {g: 1, m: 3 }
-var inputBook = "Back to school: children raise money to support #BeYouYour cause campaign at DFW(Dallas Fort Worth Area)"
+var inputRansomNote = "Hand over the chocolate and no one gets hurt"// {g: 1, m: 3 }
+var inputBook = "NYTimes online: A toddler is hurt while sliding down the chocolate slide at new year eve fair"
 
 function isRansomeNotePossible(ransomNote, book) {
     let lookUp = {}
